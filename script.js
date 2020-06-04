3686d2869647558e66f07f844b82fe84e0b84466
Anmol1804 <anmolgera01@gmail.com>
2020-06-04 23:26:00 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var ht = Number(document.querySelector("#ht").value);
+var wt = Number(document.querySelector("#wt").value);
+var result = document.querySelector("#result");
+var btn = document.querySelector+("#btn");
